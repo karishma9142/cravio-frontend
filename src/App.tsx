aa085514f0d7d526343dfebca6237c0eb35724a3
karishma9142 <karishmacair47050@gmail.com>
2026-08-27 01:53:29 +0530

    start writing restaurant page

--- src/App.tsx
+++ src/App.tsx
@@ -7,8 +7,15 @@ import PublicRoute from "./components/publicRoute";
 import SelectRole from "./pages/SelectRole";
 import Navbar from "./components/Navbar";
 import Account from "./pages/Account";
+import { useAppData } from "./context/AppContext";
+import Restaurant from "./pages/Restaurant";
 
 const App = () => {
+  const {user} = useAppData();
+
+  if(user && user.role === 'seller'){
+    return <Restaurant/>
+  }
   return <>
     <BrowserRouter>
       <Navbar />
